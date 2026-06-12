6483eb028738a2d72d0e7e6c97e79c59c8b4ac58
yudhapakel <yudahp23@gmail.com>
2026-06-12 21:11:58 +0700

    feat: add Tailwind CSS Frontend UI and Swagger API Docs support with complete Maven wrapper

---
+++ src/main/java/com/fisight/productapi/controller/ViewController.java
@@ -0,0 +1,14 @@
+package com.fisight.productapi.controller;
+
+import com.fisight.productapi.model.Product;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index.html";
+    }
+}
